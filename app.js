a61c6feb761ac98454b9d106faccd2a2b0ce8540
Tanishqa Arora <tanishqa164@gmail.com>
2022-10-25 16:18:23 +0530

    design form with node template engine

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 const port = process.env.PORT||3000;
 
 app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, 'views'));
+
+// load static assets
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 // home route
 app.get('/', (req, res) => {
