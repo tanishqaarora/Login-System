8f70f065a0627ca424badef6d21c52a0a0a23085
Tanishqa Arora <tanishqa164@gmail.com>
2022-10-25 17:14:10 +0530

    add login feature

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
+
+const router = require('./router');
 
 const app = express();
 
 const port = process.env.PORT||3000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, 'views'));
@@ -11,6 +19,14 @@ app.set('view engine', 'ejs');
 // load static assets
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+app.use(session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/route', router);
 
 // home route
 app.get('/', (req, res) => {
---
+++ router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const credential = {
+    email: "admin@gmail.com",
+    password: "admin123"
+};
+
+//login user
+router.post('/login', (req, res)=>{
+    if(req.body.email == credential.email && req.body.password == credential.password){
+        req.session.user = req.body.email;
+        // res.redirect('/dashboard');
+        res.end("Login Successful...!");
+    }else{
+        res.end("Invalid Username");
+    }
+});
+
+module.exports = router;
+
